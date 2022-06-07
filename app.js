67c8776a16fb098ac5a69a2e5b18f84f49d1ba8d
Krypto-Kazz <kryptokazz@tutanota.com>
2022-06-07 21:35:08 +1000

    hide api

--- app.js
+++ app.js
@@ -4,24 +4,30 @@ const express = require("express");
 
 const https = require("https");
 
+const magic_key = process.env.API_KEY;
+
 const app = express(); 
 
+require('dotenv').config();
+
 app.get("/", function(req,res) {
-    res.send("Server is up and running.")
-})
 
-
-const url = "https://api.openweathermap.org/data/2.5/weather?q=Austin,US&appid=APIKEY=metric"
+const url = "https://api.openweathermap.org/data/2.5/weather?q=London,UK&appid=${magic_key}&units=metric"
 
 https.get(url, (response) => {
-    console.log(response);
+    console.log(response.statusCode);
+
+response.on('data',function(data){
+    const weatherData = JSON.parse(data)
+    console.log(weatherData);
+    })
 })
 
+
+res.send("Server is up and running") 
+})
 
 app.listen(3000,function( )  {
     console.log("Sever is running on port 3000.");
 })
 
-.on('error', (error) => {
-    console.log(error);
-})
